4e518d5100184adc6a56f03deac2fd5ecb16e598
andrewheusser <andrew.heusser@gmail.com>
2016-06-20 20:22:19 -0400

    minor edits

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').Server(app);
-// var argv = require('yargs').argv;
+var port     = process.env.PORT || 8080;
 
 // Server
 app.use(express.static(path.join(__dirname, '/app')));
@@ -13,6 +13,5 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '/app/index.html'));
 });
 
-http.listen(3333, function () {
-    console.log('listening on port 3333');
-});
+http.listen(port);
+console.log('The magic happens on port ' + port);
